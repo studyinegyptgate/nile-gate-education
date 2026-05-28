151253bb1be9710dfd108618fdfcbfb284d845c7
Nile Gate Education <bnali12314@gmail.com>
2026-05-28 12:42:36 -0700

    Remove sitemap plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePluginSitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginSitemap({
-      hostname: 'https://nile-gate-education.vercel.app'
-    })
-  ]
+  plugins: [react()],
 })
